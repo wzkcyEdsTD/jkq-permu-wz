20832c765f6bd00f0366f5d4e6c75a75b2d39205
Edison_ForMan <chuanghewzh12@hotmail.com>
2020-05-21 09:58:58 +0800

    codeDesign

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
-/**
- * doinit with port on
- * update by eds 2020/4/24
- * @param {*} app
+/*
+ * @Author: eds
+ * @Date: 2020-04-27 08:50:15
+ * @LastEditTime: 2020-05-21 09:07:58
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \jkq-permu-wz\app.js
+ */ 
+/*
+ *                        _oo0oo_
+ *                       o8888888o
+ *                       88" . "88
+ *                       (| -_- |)
+ *                       0\  =  /0
+ *                     ___/`---'\___
+ *                   .' \\|     |// '.
+ *                  / \\|||  :  |||// \
+ *                 / _||||| -:- |||||- \
+ *                |   | \\\  - /// |   |
+ *                | \_|  ''\---/''  |_/ |
+ *                \  .-\__  '-'  ___/-. /
+ *              ___'. .'  /--.--\  `. .'___
+ *           ."" '<  `.___\_<|>_/___.' >' "".
+ *          | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ *          \  \ `_.   \_ __\ /__ _/   .-` /  /
+ *      =====`-.____`.___ \_____/___.-`___.-'=====
+ *                        `=---='
+ * 
+ * 
+ *      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * 
+ *            佛祖保佑       永不宕机     永无BUG
  */
 const init = async (app) => {
   app.beforeStart(async () => {
     // await app.model.sync({ force: true });
     await app.model.sync();
-    console.log("[egg.model] database on");
+    console.log("[egg.model] db on");
   });
 };
 module.exports = init;
